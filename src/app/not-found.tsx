5c255b5609281beb2499223274e468208e176477
Pavekatal <pavekatal@gmail.com>
2025-07-16 01:19:50 +0800

    реализована страница NotFoundPage

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,5 @@
+import NotFound from '@components/not-found/NotFound';
+
 export default function NotFoundPage() {
-  return (
-    <>
-      <h1>404</h1>
-      <h2>Страница не найдена</h2>
-      <p>Возможно, она была удалена или перенесена на другой адрес</p>
-    </>
-  );
+  return <NotFound />;
 }
